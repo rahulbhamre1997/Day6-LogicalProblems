32d9ccba73ef267bfe8ba5e301f932e1e76a3e8c
rahulbhamre1997 <rahul1997bhamre@gmail.com>
2022-03-09 15:25:23 +0530

    Added:Checking number is perfect or not

---
+++ src/com/logicalproblem/PerfectNumber.java
@@ -0,0 +1,30 @@
+package com.logicalproblem;
+
+import java.util.Scanner;
+
+public class PerfectNumber {
+	public static void checkPerfect() {
+		int i,  value = 0 ;
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.println(" Please Enter The Number: ");
+			int number = sc.nextInt();
+			
+			for(i = 1 ; i < number ; i++) {
+				if(number % i == 0)  {
+					value = value + i;
+				}
+			}
+			
+			if (value == number) {
+				System.out.println(+number+" is a Perfect Number");
+			}
+			else {
+				System.out.println(+number+ " is not a Perfect Number");
+			}
+		}	
+	}
+	
+	public static void main(String[] args) {
+		checkPerfect();
+	}
+}
